a23bef7b0c9a7d5ae35cb7f5d6f7fe0b9847eebb
ocowchun <ocowchun@gmail.com>
2016-08-02 11:31:14 +0800

    add example

---
+++ demo.js
@@ -0,0 +1,11 @@
+var Dictionary = require('./lib/dictionary.js');
+var files = ['example/config/locales/app.en.yml', 'example/config/locales/app.zh-TW.yml'].map(file => `${__dirname}/${file}`);
+var config = {};
+
+// /Users/ocowchun/projects/github/lococo/example/config/locales/app.en.yml
+
+Dictionary.loadDictionary(files, config).then(function(dictionary) {
+	dictionary.listKeys('');
+})
+
+// console.log(files)
