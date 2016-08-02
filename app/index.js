b42edcf72fe427c9ee157372fbe6f780703ec8e5
Kalan <kjj6198@gmail.com>
2016-08-02 12:02:59 +0800

    Revise structure.

--- app/index.js
+++ app/index.js
@@ -3,7 +3,6 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
 import App from './containers/App.js';
-import './app.global.css';
 
 const store = configureStore();
 
