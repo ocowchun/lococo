39dc1e5e1c87123859e1976825520c16e9c9bd04
Kalan <kjj6198@gmail.com>
2016-08-03 16:18:16 +0800

    Fixed MenuItemChild.

--- app/components/MenuItemChild/index.js
+++ app/components/MenuItemChild/index.js
@@ -1,23 +1,43 @@
 import React, { PropTypes } from 'react';
 import Style from './style.scss';
 
+import MenuItem from '../MenuItem/index.js';
+
 const propTypes = {
   keyName:  PropTypes.string.isRequired,
   isDir: PropTypes.bool.isRequired,
-  clickDictionary: PropTypes.func.isRequired,
 };
 
 export default class MenuItemChild extends React.Component {
   
   constructor(props) {
     super(props);
+    
+    this.state = {
+    	isDir: false,
+    	hasExpanded: false,
+    };
+  }
+
+  componentWillMount() {
+  	const curState = this.state;
+  	this.setState(
+      Object.assign({}, curState, {
+      	isDir: this.props.isDir,
+      })
+  	);
+  }
+
+  handleClick(e) {
+    const { isDir } = this.props;
+    
   }
 
   render() {
-    
+
     return (
-      <li>
-       {this.props.keyName}      	
+      <li onClick={this.handleClick.bind(this)}>
+       {this.props.keyName}
       </li>
     );
   }
